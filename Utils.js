2788417aa8b57ca910e4f147e47fb30adc22948b
Vlad Aleksandrov <wlad.aleksandrov@gmail.com>
2016-04-06 21:18:41 +0200

    Updated Web App

--- Utils.js
+++ Utils.js
@@ -1,55 +1,61 @@
-function getAuction() {
-    $.ajax({
-        url: 'http://localhost:9090/AuctionService/Auctions/' + $('#auctionId').val(),
-        success: function (auction)
-        {
-            $('#tableAuctionId').text(auction.Id);
-            $('#tableName').text(auction.Name);
-            $('#tableDescription').text(auction.Description);
-            $('#tableImageUrl').text(auction.ImageUrl);
-            $('#tableStartingPrice').text(auction.StartingPrice);
-            $('#tableHighestBidder').text(auction.HighestBidder);
-            $('#tableHighestBid').text(auction.HighestBid);
 
-            var ws = new WebSocket("ws://localhost:8089/AccountService");
+$(function () {
 
-            ws.onopen = function () {
-                ws.send("Subscribe " + $('#auctionId').val());
-            };
+    $("#enterAuction").click(getAuction);
 
-            ws.onmessage = function (evt) {
+    function getAuction() {
+        $.ajax({
+            url: 'http://localhost:9090/AuctionService/Auctions/' + $('#auctionId').val(),
+            success: function (auction) {
+                $('#tableAuctionId').text(auction.Id);
+                $('#tableName').text(auction.Name);
+                $('#tableDescription').text(auction.Description);
+                $('#tableImageUrl').text(auction.ImageUrl);
+                $('#tableStartingPrice').text(auction.StartingPrice);
+                $('#tableHighestBidder').text(auction.HighestBidder);
+                $('#tableHighestBid').text(auction.HighestBid);
 
-                var data = JSON.parse(evt.data);
-                // check if supplied value is greater than existing one
-                if(parseInt($('#tableHighestBid').text()) < data.HighestBid)
-                {     
-                     $('#tableHighestBidder').text(data.Bidder);
-                     $('#tableHighestBid').text(data.HighestBid);
-                }
-            };
-        }
-    });
-}
+                var ws = new WebSocket("ws://localhost:8089/AccountService");
 
-function placeBid() {
-    var payload = JSON.stringify( { bidderId: $('#bidderId').val(), bidAmount: $('#bidAmount').val() });
+                ws.onopen = function () {
+                    ws.send("Subscribe " + $('#auctionId').val());
+                };
 
-    $.ajax({
-        url: 'http://localhost:9090/AuctionService/Auctions/' + $('#auctionId').val(),
-        type: 'PUT',
-       // crossDomain: true,
-        contentType: "application/json",
-        data: payload,
-        processData:false,
-        success: function (data) {
+                ws.onmessage = function (evt) {
 
-            if (data)
-                $('#lastStatus').text("Last Status: SUCCESS");
-            
-            else
-                $('#lastStatus').text("Last Status: DECLINED");
-        },
-        error: function (er)
-        { }
-    });
-}
+                    var data = JSON.parse(evt.data);
+                    // check if supplied value is greater than existing one
+                    if (parseInt($('#tableHighestBid').text()) < data.HighestBid) {
+                        $('#tableHighestBidder').text(data.Bidder);
+                        $('#tableHighestBid').text(data.HighestBid);
+
+                    }
+                };
+            }
+        });
+    }
+
+    function placeBid() {
+        var payload = JSON.stringify({ bidderId: $('#bidderId').val(), bidAmount: $('#bidAmount').val() });
+
+        $.ajax({
+            url: 'http://localhost:9090/AuctionService/Auctions/' + $('#auctionId').val(),
+            type: 'PUT',
+            // crossDomain: true,
+            contentType: "application/json",
+            data: payload,
+            processData: false,
+            success: function (data) {
+
+                if (data)
+                    $('#lastStatus').text("Last Status: SUCCESS");
+
+                else
+                    $('#lastStatus').text("Last Status: DECLINED");
+            },
+            error: function (er)
+            { }
+        });
+    }
+
+});
